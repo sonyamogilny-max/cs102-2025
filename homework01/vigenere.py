cb937c9e99faf8327cc76a8349fba26fd278d8db
sonyamogilny-max <sonyamogilny@gmail.com>
2025-11-02 20:24:56 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,7 +9,21 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+
+    for i, char in enumerate(plaintext):
+        if char.isalpha():
+            key_char = keyword[i % key_length]
+            shift = ord(key_char) - ord('A')
+
+            if char.isupper():
+                encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+            else:
+                encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            ciphertext += encrypted_char
+        else:
+            ciphertext += char
     return ciphertext
 
 
@@ -24,5 +38,19 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+    key_length = len(keyword)
+
+    for i, char in enumerate(ciphertext):
+        if char.isalpha():
+            key_char = keyword[i % key_length]
+            shift = ord(key_char) - ord('A')
+
+            if char.isupper():
+                decrypted_char = chr((ord(char) - ord('A') - shift) % 26 + ord('A'))
+            else:
+                decrypted_char = chr((ord(char) - ord('a') - shift) % 26 + ord('a'))
+            plaintext += decrypted_char
+        else:
+            plaintext += char
     return plaintext
