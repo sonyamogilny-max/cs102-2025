3151aeab191a34f7f3ce527af52141d3c7e32b9a
sonyamogilny-max <sonyamogilny@gmail.com>
2025-11-02 21:18:05 +0300

    Реализована функция поиска НОД

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -32,8 +32,9 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
-    pass
+    while b != 0:
+        a, b = b, a % b
+    return abs(a)
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
@@ -54,10 +55,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p -1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
