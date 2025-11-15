440a8f8721e478accf2d6371ad0ce60117c5f4c3
sonyamogilny-max <sonyamogilny@gmail.com>
2025-11-15 12:01:38 +0300

    Реализован алгоритм аффинского шифра

--- homework01/affine.py
+++ homework01/affine.py
@@ -3,7 +3,7 @@ def encrypt_affine(plaintext, a, b):
     m = 33
     for char in plaintext:
         if char.isalpha():
-            changed_char = (a * ord(char) + b) % m
+            changed_char = chr((a * ord(char) + b) % m)
             ciphertext += changed_char
         else:
             ciphertext += char
