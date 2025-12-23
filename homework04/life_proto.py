b4fd1843c0acccd9df2647cfe9cac8ee438a5068
sonyamogilny-max <sonyamogilny@gmail.com>
2025-12-24 02:53:05 +0300

    Исправлены ошибки

--- homework04/life_proto.py
+++ homework04/life_proto.py
@@ -32,9 +32,11 @@ class GameOfLife:
     def draw_lines(self) -> None:
         """ Отрисовать сетку """
         for x in range(0, self.width, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
+            pygame.draw.line(self.screen, pygame.Color("black"),
+                             (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
-            pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
+            pygame.draw.line(self.screen, pygame.Color("black"),
+                             (0, y), (self.width, y))
 
     def run(self) -> None:
         """ Запустить игру """
@@ -63,33 +65,15 @@ class GameOfLife:
         pygame.quit()
 
     def create_grid(self, randomize: bool = False) -> Grid:
-        """
-        Создание списка клеток.
-
-        Клетка считается живой, если ее значение равно 1, в противном случае клетка
-        считается мертвой, то есть, ее значение равно 0.
-
-        Parameters
-        ----------
-        randomize : bool
-            Если значение истина, то создается матрица, где каждая клетка может
-            быть равновероятно живой или мертвой, иначе все клетки создаются мертвыми.
-
-        Returns
-        ----------
-        out : Grid
-            Матрица клеток размером `cell_height` х `cell_width`.
-        """
         if randomize:
-            grid = [[random.randint(0, 1) for _ in range(self.cell_width)] for _ in range(self.cell_height)]
+            grid = [[random.randint(0, 1) for _ in range(self.cell_width)]
+                    for _ in range(self.cell_height)]
         else:
-            grid = [[0 for _ in range(self.cell_width)] for _ in range(self.cell_height)]
+            grid = [[0 for _ in range(self.cell_width)]
+                    for _ in range(self.cell_height)]
 
         return grid
     def draw_grid(self) -> None:
-        """
-        Отрисовка списка клеток с закрашиванием их в соответствующе цвета.
-        """
         for y in range(self.cell_height):
             for x in range(self.cell_width):
                 if self.grid[y][x] == 1:
@@ -107,23 +91,6 @@ class GameOfLife:
                 )
 
     def get_neighbours(self, cell: Cell) -> Cells:
-        """
-        Вернуть список соседних клеток для клетки `cell`.
-
-        Соседними считаются клетки по горизонтали, вертикали и диагоналям,
-        то есть, во всех направлениях.
-
-        Parameters
-        ----------
-        cell : Cell
-            Клетка, для которой необходимо получить список соседей. Клетка
-            представлена кортежем, содержащим ее координаты на игровом поле.
-
-        Returns
-        ----------
-        out : Cells
-            Список соседних клеток.
-        """
         row, col = cell
         neighbours = []
 
@@ -137,15 +104,8 @@ class GameOfLife:
         return neighbours
 
     def get_next_generation(self) -> Grid:
-        """
-        Получить следующее поколение клеток.
-
-        Returns
-        ----------
-        out : Grid
-            Новое поколение клеток.
-        """
-        new_grid = [[0 for _ in range(self.cell_width)] for _ in range(self.cell_height)]
+        new_grid = [[0 for _ in range(self.cell_width)]
+                    for _ in range(self.cell_height)]
 
         for y in range(self.cell_height):
             for x in range(self.cell_width):
